412964953bb32c8762ebfc74535a86481904ecab
Ryan Wells <rawells14@gmail.com>
2015-07-15 16:59:25 -0500

    Various Enhancements

--- MyApplication/app/src/main/java/com/rawells/uilpractice/HomeScreen.java
+++ MyApplication/app/src/main/java/com/rawells/uilpractice/HomeScreen.java
@@ -54,15 +54,18 @@ public class HomeScreen extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
+                mTitle = getString(R.string.home);
+                        break;
+            case 2:
                 mTitle = getString(R.string.title_section1);
                 break;
-            case 2:
+            case 3:
                 mTitle = getString(R.string.title_section2);
                 break;
-            case 3:
+            case 4:
                 mTitle = getString(R.string.title_section3);
                 break;
-            case 4:
+            case 5:
                 mTitle = getString(R.string.title_section4);
                 break;
         }
--- MyApplication/app/src/main/java/com/rawells/uilpractice/NavigationDrawerFragment.java
+++ MyApplication/app/src/main/java/com/rawells/uilpractice/NavigationDrawerFragment.java
@@ -102,6 +102,7 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
+                        getString(R.string.home),
                         getString(R.string.title_section1),
                         getString(R.string.title_section2),
                         getString(R.string.title_section3),
