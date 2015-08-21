a6a4c20d88a2298656e48a1fd441b07e0e1e21b7
Ryan Wells <rawells14@gmail.com>
2015-08-20 21:43:57 -0500

    Started Application

--- app/app.py
+++ app/app.py
@@ -1,2 +1,14 @@
-import flask
+from flask import Flask
+from flask import render_template
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    app.debug = True;
+    app.run()
