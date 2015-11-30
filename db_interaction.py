7e6dff6adb05ed62dcadf6034157b52f9c4d275c
Ryan Wells <rawells14@gmail.com>
2015-11-30 11:33:03 -0600

    Sqlite?

--- db_interaction.py
+++ db_interaction.py
@@ -4,12 +4,13 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import *
+
 Base = declarative_base()
-#mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
+# mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
 
 sqlite_database = 'sqlite:///' + 'data.db'
 mysql_database = 'mysql://root:ryan@localhost/uilpractice'
-engine = create_engine(mysql_database, echo=False)
+engine = create_engine(sqlite_database, echo=False)
 print(engine)
 
 Session = sessionmaker(bind=engine)
