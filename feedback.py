4cbdb4a6ef69a47ce4c3d1cddf431b39bf4e40d5
Ryan Wells <rawells14@gmail.com>
2015-12-05 11:32:37 -0600

    feedback system

--- feedback.py
+++ feedback.py
@@ -1,3 +1,4 @@
 def new_feedback(name, message):
     file = open('feedback.txt', "a")
     file.write(name + '  wrote:  ' + message + '\n'+'***********************************'+'\n\n')
+    file.close()
