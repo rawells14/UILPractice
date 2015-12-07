9b84a27287b405bb3272c3f1fa14370a4a550116
Ryan Wells <rawells14@gmail.com>
2015-12-07 12:51:33 -0600

    Feedback and ignore

--- feedback.py
+++ feedback.py
@@ -4,5 +4,5 @@ import settings
 def new_feedback(name, message):
     file = open(settings.PROJECT_PATH + 'feedback.txt', "a")
     file.write(
-        name + '  wrote:  \n' + message + '\n' + '-------------------------------------------' + '\n\n' + '-------------------------------------------')
+        name + ' wrote:\n\n' + message + '\n----------------------------------------------------------------\n')
     file.close()
