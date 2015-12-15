78fe307c19fe43be06220c600ec6e1858e58db53
Ryan Wells <rawells14@gmail.com>
2015-12-15 11:22:27 -0600

    poolilng

--- db_interaction.py
+++ db_interaction.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 # mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
 
 data_base_address = settings.DB_ADDRESS
-engine = create_engine(data_base_address, echo=False, pool_recycle=3600)
+engine = create_engine(data_base_address, echo=False, pool_recycle=2)
 
 Session = sessionmaker(bind=engine, autoflush=True)
 session = Session()
