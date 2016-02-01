dc4821a82b7fe23a03594b304bedf07cabcdc15a
Ryan Wells <rawells14@gmail.com>
2016-02-01 13:11:10 -0600

    Picture System on Homepage

--- static/analytics.js
+++ static/analytics.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     });
 });
 
-
 function graphData(correct, incorrect){
 var pieData = [{
     value: correct,
@@ -21,8 +20,6 @@ var pieData = [{
     var ctx = document.getElementById("correct-incorrect-chart").getContext("2d");
     var correctIncorrectChart = new Chart(ctx).Pie(pieData);
 }
-
-
 function getColor(percentage){
     var colors = ["#AA3939", "#AA6939", "#AA9239", "#68C21D", "#148282"];
     var percents = [50, 60, 70, 80, 90]
