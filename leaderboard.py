ed9b23670169142ad512f5d4ba52748bc1ef2351
Ryan Wells <rawells14@gmail.com>
2016-03-08 11:52:46 -0600

    Math roudning

--- leaderboard.py
+++ leaderboard.py
@@ -22,11 +22,10 @@ def get_top_ten_accuracy():
     for user in users:
         if (user.totalattempted > 10):
             usernames.append(user.username)
-            accuracies.append((str)(round(100 * (user.totalcorrect / user.totalattempted), 2)))
-            print('ACCURACY: ' + (str)(round(100 * (user.totalcorrect / user.totalattempted), 2)))
+            accuracies.append((user.totalcorrect / user.totalattempted * 100))
             tot += 1
-        if (tot >= 10):
-            break
+            if (tot >= 10):
+                break
     data = [usernames, accuracies]
     return data
 
